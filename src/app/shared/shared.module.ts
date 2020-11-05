d9e1cbd45ac259f222da7f778542915e68b068d6
theyanniss23002 <mr.artur23002@gmail.com>
2020-11-05 03:06:15 +0300

    Structure home page improvement

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,19 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SharedRoutingModule} from './shared-routing.module';
-import {SwiperComponent} from './swiper/swiper.component';
 import {SwiperModule} from "ngx-swiper-wrapper";
 import {AuthModule} from "../auth/auth.module";
 
 
 @NgModule({
-  declarations: [SwiperComponent],
+  declarations: [],
   imports: [
     CommonModule,
     SharedRoutingModule,
     SwiperModule,
     AuthModule
   ],
-    exports: [SwiperComponent]
+    exports: []
 })
 export class SharedModule {
 }
